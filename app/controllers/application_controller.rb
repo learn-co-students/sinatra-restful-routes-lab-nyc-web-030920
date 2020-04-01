3070e015c324bef296fa06e6eb6d3c2290edb2d6
MisterC1ean <robert.keller22@gmail.com>
2020-04-01 10:27:14 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,29 +8,20 @@ class ApplicationController < Sinatra::Base
 
 #GET form for NEW recipe
 
+  get "/recipes/new" do
+    erb :new
+  end
+
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
 
-
-  get "/recipes/new" do
-    Recipe.new(params)
-    erb :new
-  end
-
-  post "/recipes" do 
-    @recipe = Recipe.create(params)
-
-    redirect to :show
-  end
-
   get "/recipes/:id" do
     @recipe = Recipe.find(params[:id])
-
     erb :show
   end
-
+  
   get "/recipes/:id/edit" do
     @recipe = Recipe.find(params[:id])
 
@@ -43,11 +34,20 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    redirect to :show
+    redirect to "/recipes/#{@recipe.id}"
   end
 
+  post "/recipes" do 
+    @recipe = Recipe.create(params)
+    redirect to "recipes/#{@recipe.id}"
+  end
+
+
+
+
   delete "/recipes/:id" do
-    Recipe.destroy(params[:id])
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
 
     redirect to '/recipes'
   end
