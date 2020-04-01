06d3f28ea6e67e9538e13d142f627c5f7c9fe648
CBreakr <github email address>
2020-03-31 22:43:04 -0400

    Done.

---
+++ db/migrate/20200401021800_create_recipes.rb
@@ -0,0 +1,10 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |table|
+      table.string :name
+      table.string :ingredients
+      table.string :cook_time
+      table.timestamps
+    end
+  end
+end
